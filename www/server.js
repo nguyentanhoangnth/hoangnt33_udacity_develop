ee8806cf088bb914426379508c72d9c0772ce7fe
Hoang Nguyen <nguyentanhoang.nth@gmail.com>
2022-04-18 20:56:22 +0700

    Update Infor Deploy EB Environtment

---
+++ www/server.js
@@ -0,0 +1,49 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const util_1 = require("./util/util");
+(() => __awaiter(this, void 0, void 0, function* () {
+    // Init the Express application
+    const app = express_1.default();
+    // Set the network port
+    const port = process.env.PORT || 8080;
+    // Use the body parser middleware for post requests
+    app.use(body_parser_1.default.json());
+    // filter image
+    app.get("/api/filteredimage/", (req, res) => __awaiter(this, void 0, void 0, function* () {
+        let { image_url } = req.query;
+        if (!image_url || image_url == "") {
+            return res.status(400).send({ message: "Image Url is required or malformed" });
+        }
+        const resultUrl = yield util_1.filterImageFromURL(image_url);
+        if (!resultUrl || resultUrl == "") {
+            return res.status(201).send({ message: "Have no any resources can be find" });
+        }
+        // delete local file
+        res.on('finish', () => util_1.deleteLocalFiles([resultUrl]));
+        return res.status(200).send({ message: "Get image successfully", url: resultUrl });
+    }));
+    // Root Endpoint
+    // Displays a simple message to the user
+    app.get("/", (req, res) => __awaiter(this, void 0, void 0, function* () {
+        res.send("try GET /filteredimage?image_url={{}}");
+    }));
+    // Start the Server
+    app.listen(port, () => {
+        console.log(`server running http://localhost:${port}`);
+        console.log(`press CTRL+C to stop server`);
+    });
+}))();
+//# sourceMappingURL=server.js.map
